a564df7a026fb4b76d13858c16ca3d9c29e6a867
Dmytro Buhai <buhai@nixsolutions.com>
2024-03-24 22:45:35 +0100

    changing program to default class

--- WebScrapingApp/Program.cs
+++ WebScrapingApp/Program.cs
@@ -1,50 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-using NLog;
+﻿using NLog;
 using SeleniumScraper;
 using SeleniumScraper.Commands;
 
-Logger logger = LogManager.GetCurrentClassLogger();
-var defaultForegroundColor = Console.ForegroundColor;
-
-
-logger.Info("Program starting...");
-
-var edgeLauncher = new EdgeLauncher(logger);
-
-var twitterURL = "https://twitter.com/home";
-
-var url = WriteMessageWaitForInputData("Enter URL to navigate: ");
-
-var navigateCommand = new NavigateCommand(edgeLauncher.GetEdgeDriver(), logger, url);
-
-navigateCommand.Execute();
-
-Thread.Sleep(5000);
-
-edgeLauncher.Dispose();
-
-string WriteMessageWaitForInputData(string message)
+public class Program
 {
-    WriteMessage(message);
-    return ReadInput();
-}
+    private static readonly ConsoleColor DefaultConsoleForegroundColor = Console.ForegroundColor;
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-void WriteMessage(string message)
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.Write(message);
-}
-
-string ReadInput()
-{
-    var input = Console.ReadLine();
-
-    while (string.IsNullOrEmpty(input))
+    private static void Main(string[] args)
     {
-        Console.Write("Data is empty, try again: ");
-        input = Console.ReadLine();
+        Logger.Info("Program starting...");
+
+        var edgeLauncher = new EdgeLauncher(Logger);
+
+        //var twitterURL = "https://twitter.com/home";
+
+        var url = WriteMessageWaitForInputData("Enter URL to navigate: ");
+
+        var navigateCommand = new NavigateCommand(edgeLauncher.GetEdgeDriver(), Logger, url);
+
+        navigateCommand.Execute();
+
+        Thread.Sleep(5000);
+
+        edgeLauncher.Dispose();
     }
 
-    Console.ForegroundColor = defaultForegroundColor;
-    return input;
+    public static string WriteMessageWaitForInputData(string message)
+    {
+        WriteMessage(message);
+        return ReadInput();
+    }
+
+    private static void WriteMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(message);
+    }
+
+    private static string ReadInput()
+    {
+        var input = Console.ReadLine();
+
+        while (string.IsNullOrEmpty(input))
+        {
+            Console.Write("Data is empty, try again: ");
+            input = Console.ReadLine();
+        }
+
+        Console.ForegroundColor = DefaultConsoleForegroundColor;
+        return input;
+    }
 }
